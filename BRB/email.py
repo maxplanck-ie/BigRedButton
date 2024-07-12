5a6ea22218ab27b77cfcbbfd0e789f8e47767173
WardDeb <ward@deboutte.be>
2024-07-12 11:22:14 +0200

    rework email to have a more meaningfull table, add deepseq if samba is involved

--- BRB/email.py
+++ BRB/email.py
@@ -1,6 +1,10 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from importlib.metadata import version
+from dominate.tags import html, div, br
+from tabulate import tabulate
+from BRB.logger import log
 
 def errorEmail(config, errTuple, msg) :
     msg = MIMEText(msg + "\nError type: %s\nError value: %s\n%s\n" % (errTuple[0], errTuple[1], errTuple[2]))
@@ -13,15 +17,41 @@ def errorEmail(config, errTuple, msg) :
     s.quit()
 
 
-def finishedEmail(config, msg) :
-    message = "Flow cell: {}\n".format(config.get("Options","runID"))
-    message += msg
+def finishedEmail(config, msg):
+    mailer = MIMEMultipart('alternative')
+    mailer['Subject'] = f"[BigRedButton {version("BRB")}] {config.get("Options","runID")} processed"
+    mailer['From'] = config.get("Email","fromAddress")
+    
+    # Create the table head
+    _html = html()
+    # Default recipient is finishedTo (bioinfocore)
+    recipient = config.get("Email","finishedTo")
+    # Inform deepseq too if we have a sambaUpdate:
+    if any([i[6] for i in msg]):
+        log.info("At least one sambaUpdate true in msg")
+        # Only inform deepseq if all workflows are succesfull (combat spam)
+        if [i[4] for i in msg].count('success') == len(msg):
+            recipient = config.get("Email","deepSeq")
+            _html.add(div(
+                "Post-processing is ready, deepSeq's sambda drive is updated for at least one project.",
+                br()
+            ))
+    
+    mailer['To'] = recipient
+    # Table
+    tabHead = ['Project', 'organism', 'libraryType', 'workflow', 'workflow_status', 'parkour_status', 'sambaUpdate']
+    message = tabulate(
+        msg, tabHead, tablefmt="html", disable_numparse=True
+    )
 
-    msg = MIMEText(message)
-    msg['Subject'] = f"[BigRedButton {version("BRB")}] {config.get("Options","runID")} processed"
-    msg['From'] = config.get("Email","fromAddress")
-    msg['To'] = config.get("Email","finishedTo")
-
-    s = smtplib.SMTP(config.get("Email","host"))
-    s.send_message(msg)
+    email = MIMEText(message, 'html')
+    mailer.attach(email)
+    
+    s = smtplib.SMTP(config.get("Email", "host"))
+    
+    s.sendmail(
+        config.get("Email","fromAddress"),
+        recipient.split(','),
+        mailer.as_string()
+    )
     s.quit()
