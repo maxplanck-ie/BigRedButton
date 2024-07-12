120688fc97ee18569b8714b00f952bb592c2dcec
WardDeb <ward@deboutte.be>
2024-07-12 11:22:31 +0200

    fix off-species rate

--- BRB/ET.py
+++ BRB/ET.py
@@ -6,7 +6,7 @@ import json
 import BRB.misc
 from BRB.logger import log
 import pandas as pd
-
+from pathlib import Path
 
 def getNReads(d):
     """
@@ -56,7 +56,9 @@ def getOffSpeciesRate(d, organism = None) -> float:
     with open(fname) as f:
         for line in f:
             if org_map[organism] in line:
-                off = float(line.strip().split()[0])/100
+                off = 1-(float(line.strip().split()[0])/100)
+    # off-species actually means fraction of non-expected organism reads !
+    # off-species reads vs of-species reads ;)
     log.info(f"confident reads for {fname} = {off}")
     return off
 
@@ -198,9 +200,11 @@ def sendToParkour(config, msg):
     log.info(f"sendToParkour: Sending {d} to Parkour")
     res = requests.post(config.get("Parkour", "ResultsURL"), auth=(config.get("Parkour", "user"), config.get("Parkour", "password")), data=d, verify=config.get("Parkour", "cert"))
     log.info(f"sendToParkour return {res}")
+    return res
 
 
-def phoneHome(config, outputDir, pipeline, samples_tuples, organism):
+
+def phoneHome(config, outputDir, pipeline, samples_tuples, organism, project, libType):
     """
     Return metrics to Parkour, the results are in outputDir and pipeline needs to be run on them
     """
@@ -224,26 +228,32 @@ def phoneHome(config, outputDir, pipeline, samples_tuples, organism):
         msg = m
 
     if msg is not None:
-        sendToParkour(config, msg)
+        ret = sendToParkour(config, msg)
+    else:
+        ret = None
+    
+    return [project, organism, libType, pipeline, 'success', ret]
 
 
 def telegraphHome(config, group, project, skipList, organism=None):
     """
     The skipList is a list of samples/libraries for which we don't run a pipeline, but it'd be nice to still send back sequencing metrics
-
-    ([library, sampleName])
+    Structure of skipList:
+    [library, sampleName, libraryType]
     """
+    log.info(f"telegraphHome triggered for {project}")
     # make a fake output directory path
-    baseDir = "{}/{}/{}/{}/Analysis_{}".format(config.get('Paths', 'groupData'),
-                                                            BRB.misc.pacifier(group),
-                                                            BRB.misc.getLatestSeqdir(config.get('Paths','groupData'), group),
-                                                            config.get('Options', 'runID'),
-                                                            BRB.misc.pacifier(project))
-    outputDir = os.path.join(baseDir, "DNA_mouse") # does not matter what it is, it is just a generic name. No pipeline is run on these data
+    baseDir = Path(
+        config.get('Paths', 'groupData'),
+        BRB.misc.pacifier(group),
+        BRB.misc.getLatestSeqdir(config.get('Paths','groupData'), group),
+        config.get('Options', 'runID'),
+        BRB.misc.pacifier(project)
+    )
+    # Mock path
+    outputDir = baseDir / "DNA_mouse"
     samples_id = [row[0] for row in skipList]
-    print("SAMPLESID = {}".format(samples_id))
     baseDict, sample2lib = getBaseStatistics(config, outputDir, samples_id, organism)
-    print("telegraphHome: baseDict: {}, sample2lib: {}".format(baseDict, sample2lib))
     # Reformat into a matrix
     m = []
     for k, v in baseDict.items():
@@ -251,5 +261,8 @@ def telegraphHome(config, group, project, skipList, organism=None):
                   'reads_pf_sequenced': v[1],
                   'confident_reads': v[2],
                   'optical_duplicates': v[3]})
-    sendToParkour(config, m)
-    return "Sent information on {} libraries from project {} from the {} group to Parkour\n".format(len(skipList), project, group)
+    ret = sendToParkour(config, m)
+    # Format the libtypes
+    libTypes = ','.join(set([i[2] for i in skipList]))
+    # [project, organism, libtypes, workflow, workflow status, parkour status, sambaUpdate]
+    return [project, organism, libTypes, None, None, ret, False]
