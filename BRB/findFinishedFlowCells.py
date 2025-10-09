c4f776e854785387aaf43fd5b3ba0aa56d121d07
NavandarM <navandar@ie-freiburg.mpg.de>
2025-10-09 16:57:44 +0200

    Aviti enable for querying in parkour

--- BRB/findFinishedFlowCells.py
+++ BRB/findFinishedFlowCells.py
@@ -24,22 +24,44 @@ def markFinished(config):
     print(f"{_p} created, flow cell processed.")
 
 
+# def queryParkour(config):
+#     print(config.get("Options", "runID"))
+#     FCID = config.get("Options", "runID").split("_")[3][1:]  # C605HACXX from 150416_SN7001180_0196_BC605HACXX
+#     if '-' in FCID:
+#         FCID = FCID.split('-')[-1]
+#     d = {'flowcell_id': FCID}
+#     print(d)
+#     res = requests.get(config.get("Parkour", "QueryURL"), auth=(config.get("Parkour", "user"), config.get("Parkour", "password")), params=d, verify=config.get("Parkour", "cert"))
+#     if res.status_code == 200:
+#         return res.json()
+#     return dict()
+
 def queryParkour(config):
-    FCID = config.get("Options", "runID").split("_")[3][1:]  # C605HACXX from 150416_SN7001180_0196_BC605HACXX
-    if '-' in FCID:
-        FCID = FCID.split('-')[-1]
-    d = {'flowcell_id': FCID}
+    basePath= config.get("Paths","baseData")
+    aviti_check= glob.glob(f"{basePath}/*/RunManifest.csv")
+    if aviti_check:
+        FCID = config.get("Options", "runID").split("_")[2]
+        if '-' in FCID:
+            FCID = FCID.split('-')[-1]
+        d = {'flowcell_id': FCID}
+    else:
+        print(config.get("Options", "runID"))
+        FCID = config.get("Options", "runID").split("_")[3][1:]  # C605HACXX from 150416_SN7001180_0196_BC605HACXX
+        if '-' in FCID:
+            FCID = FCID.split('-')[-1]
+        d = {'flowcell_id': FCID}
     res = requests.get(config.get("Parkour", "QueryURL"), auth=(config.get("Parkour", "user"), config.get("Parkour", "password")), params=d, verify=config.get("Parkour", "cert"))
     if res.status_code == 200:
         return res.json()
     return dict()
 
-
 def newFlowCell(config):
     dirs = glob.glob("{}/*/fastq.made".format(config.get("Paths","baseData")))
+    print(dirs)
     for d in dirs :
         #Get the flow cell ID (e.g., 150416_SN7001180_0196_BC605HACXX)
-        config.set('Options','runID',d.split("/")[-2])
+        print(config.set('Options','runID',d.split("/")[-2]))
+        
 
         if config.get("Options","runID")[:4] < "1804":
             continue
